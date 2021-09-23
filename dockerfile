5c4bd89b570696159052114ac83e1e4008b9cfb4
sf726 <73407100+sf726@users.noreply.github.com>
2021-09-22 22:38:16 -0400

    adding client that works

--- dockerfile
+++ dockerfile
@@ -1,16 +1,7 @@
 # syntax=docker/dockerfile:1
 
-FROM openjdk:11-alpine3.13
+FROM openjdk:11-jre-slim
 
-WORKDIR /app
+COPY target/demo-0.0.1-SNAPSHOT.jar /helloworld.jar
 
-COPY .mvn/ .mvn
-COPY mvnw pom.xml ./
-RUN ./mvnw dependency:go-offline
-
-COPY src ./src
-
-EXPOSE 8080
-EXPOSE 8083
-
-CMD ["./mvnw", "spring-boot:run"]
+ENTRYPOINT java -jar helloworld.jar
---
+++ src/main/java/com/Test.java
@@ -0,0 +1,94 @@
+package com;
+
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
+import io.grpc.netty.shaded.io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+
+import javax.net.ssl.TrustManagerFactory;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A simple client that requests a greeting from the {@link HelloWorldServer}.
+ */
+public class Test {
+    private static final Logger logger = Logger.getLogger(Test.class.getName());
+
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    /** Construct client for accessing HelloWorld server using the existing channel. */
+    public Test(Channel channel) {
+        // 'channel' here is a Channel, not a ManagedChannel, so it is not this code's responsibility to
+        // shut it down.
+
+        // Passing Channels to code makes code easier to test and makes it easier to reuse Channels.
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    /** Say hello to server. */
+    public void greet(String name) {
+        logger.info("Will try to greet " + name + " ...");
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloReply response;
+        try {
+            response = blockingStub.sayHello(request);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+        logger.info("Greeting: " + response.getMessage());
+    }
+
+    /**
+     * Greet server. If provided, the first element of {@code args} is the name to use in the
+     * greeting. The second argument is the target server.
+     */
+    public static void main(String[] args) throws Exception {
+        String user = "world";
+        // Access a service running on the local machine on port 50051
+        String target = "localhost:50051";
+        // Allow passing in the user and target strings as command line arguments
+        if (args.length > 0) {
+            if ("--help".equals(args[0])) {
+                System.err.println("Usage: [name [target]]");
+                System.err.println("");
+                System.err.println("  name    The name you wish to be greeted by. Defaults to " + user);
+                System.err.println("  target  The server to connect to. Defaults to " + target);
+                System.exit(1);
+            }
+            user = args[0];
+        }
+        if (args.length > 1) {
+            target = args[1];
+        }
+
+        // Create a communication channel to the server, known as a Channel. Channels are thread-safe
+        // and reusable. It is common to create channels at the beginning of your application and reuse
+        // them until the application shuts down.
+        ManagedChannel channel  = NettyChannelBuilder
+                .forAddress("35.239.252.10",443)
+                .sslContext(GrpcSslContexts.forClient().trustManager(new File("")).build())
+                .overrideAuthority("grpc.example.com")
+                .build();
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+        try {
+            Test client = new Test(channel);
+            client.greet(user);
+        } finally {
+            // ManagedChannels use resources like threads and TCP connections. To prevent leaking these
+            // resources the channel should be shut down when it will no longer be used. If it may be used
+            // again leave it running.
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
+    }
+}
